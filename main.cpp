84844413dfc5dec7be59dcabacdb54fe620447b1
Quang Huy <huy.tranquang116@gmail.com>
2017-05-14 23:41:16 +0700

    gui bai tap 2 cho a tien

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Tran Quang Huy!" << endl;
+    cout << "Hello Cat\n";
+    cout << "Hello Dog\n";
+    cout << "Hello Elephent\n";
+    return 0;
+}
